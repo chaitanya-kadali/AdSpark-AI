37aac561f771fbcf059b37ad7a77ea84cdeeef65
chaitanya-kadali <charankadaliaa@gmail.com>
2025-03-18 23:43:03 +0530

    added fb link in react comp at app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,30 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './components/Home';
 import Thankyou from './components/Thankyou';
 
+const NoScriptFallback = () => (
+  
+<noscript>
+  <img height="1" width="1" 
+  style={{ display: "none" }}
+  src="https://www.facebook.com/tr?id=604992212362592&ev=PageView&noscript=1"
+  alt="Tracking Pixel"/>
+</noscript>
+
+);
+
 const App = () => {
   return (
   <>
+
+  <NoScriptFallback />
+
    <BrowserRouter> 
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/thankyou' element={ <div> <Thankyou/> </div> }/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/thankyou' element={ <div> <Thankyou/> </div> }/>
+        </Routes>
     </BrowserRouter>  
+
   </>
   
   )
