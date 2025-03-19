3dfbc659dfba994337112c13f2884475b8b7d001
chaitanya-kadali <charankadaliaa@gmail.com>
2025-03-19 22:27:38 +0530

    added fb link in indirect mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './components/Home';
 import Thankyou from './components/Thankyou';
-import MetaScript from '../MetaScript';
 
+import MetaScript from '../MetaScript';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
   <>
 
   {/* <NoScriptFallback /> */}
-  <MetaScript />
-
+  {/* <MetaScript /> */}
    <BrowserRouter> 
         <Routes>
           <Route path='/' element={<Home/>}/>
